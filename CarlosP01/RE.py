a84fb9cf7bec089811d2565ff05f8767bc625d32
carlosenriquejavierdelarosa <carlosjjavier05@gmail.com>
2025-05-07 22:18:46 -0400

    #this is for regular expression

---
+++ CarlosP01/RE.py
@@ -0,0 +1,19 @@
+#this is for regular expression
+import re
+
+file = open('regex_sum_2209539.txt')
+sun = []
+sun2 = 0
+for line in file:
+    line = line.strip()
+    nums = re.findall('([0-9]+)', line)
+    for num in nums: #to be able to only print the digit I need to do a for to go thr each one of them.
+        sun.append(int(num))
+
+
+for digit in sun:
+    if digit >= 0:
+        sun2 += digit
+
+print(sun2)
+
